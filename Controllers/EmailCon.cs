a915d4d233c05290513cb0fa28f1b790525ee3fa
ananyacloud4code <atripathi@cloud4code.com>
2022-08-26 17:59:20 +0530

    commit

---
+++ Controllers/EmailCon.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace EmailSendgrid.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailCon : ControllerBase
+    {
+        private readonly ISendGridClient _sendGridClient;
+        private readonly IConfiguration _configuration;
+        public EmailCon(
+            ISendGridClient sendGridClient,
+            IConfiguration configuration)
+        {
+            _sendGridClient = sendGridClient;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        [Route("send-text-mail")]
+        public async Task<IActionResult> SendPlainTextEmail(string toEmail)
+        {
+            string fromEmail = _configuration.GetSection("SendGridEmailSettings")
+            .GetValue<string>("FromEmail");
+
+            string fromName = _configuration.GetSection("SendGridEmailSettings")
+            .GetValue<string>("FromName");
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(fromEmail, fromName),
+                Subject = "Check Message",
+                PlainTextContent = "Hello, have a good day :)!!!"
+            };
+            msg.AddTo(toEmail);
+            var response = await _sendGridClient.SendEmailAsync(msg);
+            string message = response.IsSuccessStatusCode ? "Email Send Successfully" :
+            "Email Sending Failed";
+            return Ok(message);
+        }
+    }
+}
